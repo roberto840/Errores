e58ec4f29d9887f6eb7ee01af9f2eb3229038720
Agustin Diaz <Agustin Diaz@AgustinDiaz-PC>
2021-03-10 08:14:16 -0600

    Programa para calcular el error absoluto,relativo y relativo porcentual

---
+++ src/programaerrores/ProgramaErrores.java
@@ -0,0 +1,17 @@
+package programaerrores;
+import java.util.Scanner;
+public class ProgramaErrores {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double ValorReal,ValorAproximado;
+        System.out.println("Ingrese el valor real:");
+        ValorReal=teclado.nextDouble();
+        System.out.println("Ingrese el valor aproximado:");
+        ValorAproximado=teclado.nextDouble();
+        Errores error1=new Errores(ValorReal,ValorAproximado);
+        System.out.println("El error absoluto es: "+error1.ErrorAbsoluto());
+        System.out.println("El error relativo es: "+error1.ErrorRelativo());
+        System.out.println("El error relativo porcentual es: "+error1.ErrorRelativoPorcentual());
+    }
+    
+}
